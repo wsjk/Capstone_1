fbb5b2745bebdcb54e71ee8138b55e1993a7d985
wsjk <schoolsofthought@gmail.com>
2018-01-28 19:25:49 -0700

    added some more functionality to get_plotly. made some histograms and pdfs

--- src/tools/data_vis.py
+++ src/tools/data_vis.py
@@ -10,18 +10,19 @@ import cufflinks as cf
 init_notebook_mode(connected=True)
 cf.go_offline()
 
-def get_plotly(dfs, x, y="", dfname=[], xlabel='', ylabel="", text = 'title', size="", plot_type='scatter', title=""):
+def get_plotly(dfs, x, y="", dfname="", xlabel='', ylabel="", text = 'title', size="", plot_type='scatter', title="", histnorm='probability'):
+    if not dfname:
+        dfname = ['series {}'.format(i) for i in range(len(dfs))]
     if plot_type == 'scatter':
-        
         data = [{'x':df[x], 
                 'y':df[y], 
                 'mode':'markers', 
                 'text':df.reset_index()[text],
                 'marker':dict(size=df[size] if size else ""),
                 'name':dfname}  
-                for df,dfname in zip(dfs, dfname)]
+                for df, dfname in zip(dfs, dfname)]
 
-        layout = go.Layout(title=title if title else x + ' vs ' + y,
+        layout = go.Layout(title=title if title else y + ' vs ' + x,
             xaxis=dict(title=xlabel if xlabel else x),
             yaxis=dict(title=ylabel if ylabel else y)
             )
@@ -32,7 +33,9 @@ def get_plotly(dfs, x, y="", dfname=[], xlabel='', ylabel="", text = 'title', si
         # distribution of Hits and Flops by budget
 
         data = [go.Histogram(x=df[x],
-                        name=dfname) for df, dfname in zip(dfs,dfname)]
+                        name=dfname,
+                        histnorm = 'probability'
+                        ) for df, dfname in zip(dfs,dfname)]
 
         layout = go.Layout(title=title if title else 'histogram of ' + x, xaxis=dict(title=xlabel if xlabel else x),
                         yaxis=dict(title=ylabel  if ylabel else y))
