dea29cd773163d7ef560dd293611bc9d44862d27
wsjk <schoolsofthought@gmail.com>
2018-03-28 22:44:06 -0600

    added actor gender to Genres.csv

--- src/data/prep_data_for_models.py
+++ src/data/prep_data_for_models.py
@@ -10,6 +10,20 @@ save_path = os.path.abspath(os.path.join(current_file_path, 'notebooks'))
 sys.path.append(os.path.abspath(os.path.join(tools_path)))
 import eda_tools as eda
 
+def get_data():
+    import_data_path = os.path.join(current_file_path,'src','data')
+    spec = importlib.util.spec_from_file_location("import_clean_csv", os.path.join(import_data_path,"import_clean_data.py"))
+    import_data = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(import_data)
+    data = import_data.import_clean_csv(current_file_path)
+    return data
+
+def get_movies(data):
+    # create dataframes from cleaned data
+    movies = data['tmdb_movie_main'].set_index(['movie_id', 'title'])
+    movies['release_date'] = pd.to_datetime(movies['release_date'])
+    movies['net'] = movies['revenue'] - movies['budget']
+    return movies
 
 #get data for actor filmography
 def get_actor_credits(data, movies, save_csv):
@@ -17,10 +31,14 @@ def get_actor_credits(data, movies, save_csv):
     first_billing = cast.groupby(['movie_id', 'title'], as_index=False).first()
     first_billing.set_index(['movie_id', 'title'], inplace=True)
     movies_with_actors = movies.merge(first_billing, left_index=True, right_index=True)
+    
+    gender = pd.get_dummies(movies_with_actors['gender'])
+    gender.columns = ['unknown', 'female','male']
+    movies_with_actors = movies_with_actors.merge(gender, left_index=True, right_index=True)
     actors_df = movies_with_actors.reset_index()
     binned_actors = eda.get_rolling_history(actors_df, 'name', new_col='credits', func=len, func_col='title')
     binned_actors = eda.get_rolling_history(actors_df, 'name', new_col='net_to_date', func=sum, func_col='net')
-    new_df = binned_actors[['movie_id', 'title', 'name', 'gender', 'credits', 'net_to_date']]
+    new_df = binned_actors[['movie_id', 'title', 'name', 'unknown','male','female', 'credits', 'net_to_date']]
     if save_csv:
         new_df.to_csv(os.path.abspath(os.path.join(save_path,'Actor_credits.csv')), index=False)
 
@@ -44,32 +62,23 @@ def get_crew_credits(data, movies, save_csv):
         dfs[job] = new_df
     return dfs
 
-def get_data():
-    import_data_path = os.path.join(current_file_path,'src','data')
-    spec = importlib.util.spec_from_file_location("import_clean_csv", os.path.join(import_data_path,"import_clean_data.py"))
-    import_data = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(import_data)
-    data = import_data.import_clean_csv(current_file_path)
-    return data
-
-def get_movies(data):
-    # create dataframes from cleaned data
-    movies = data['tmdb_movie_main'].set_index(['movie_id', 'title'])
-    movies['release_date'] = pd.to_datetime(movies['release_date'])
-    movies['net'] = movies['revenue'] - movies['budget']
-    return movies
-
-def get_filmography():
-	movies = get_movies()
-	actor_df = get_actor_credits(movies, save_csv=False)
-	crew_dfs = get_crew_credits(movies, save_csv=False)
-
+def get_genre_data(data,movies,save_csv):
+    genres = data['tmdb_movie_genres'].set_index(['movie_id', 'title'])
+    genres = genres.drop('id',axis=1)
+    genre_count = genres.reset_index().groupby(['movie_id', 'title']).count()
+    genre_count.columns = ['genre_count']
+    genre_dummies = pd.get_dummies(genres['name']).reset_index().groupby(['movie_id', 'title']).sum()
+    genre_data = genre_dummies.merge(genre_count, left_index=True, right_index=True)
+    if save_csv:
+        genre_data.to_csv('Genres.csv')
+    return genre_data
 
 if __name__ == "__main__":
     data = get_data()
     movies = get_movies(data)
-    crew_dfs = get_crew_credits(data, movies, save_csv=True)
+    #crew_dfs = get_crew_credits(data, movies, save_csv=True)
     actor_df = get_actor_credits(data, movies, save_csv=True)
+    #genre_df = get_genre_data(data, movies, save_csv=True)
 
 
 	
